798aa305116e4592e385e496b49ba1a7db59ce41
pikapikamaya <38804662+pikapikamaya@users.noreply.github.com>
2018-06-19 11:35:45 -0700

    Add files via upload

---
+++ lab2a/lab2_add.c
@@ -0,0 +1,296 @@
+// NAME: Yanyin Liu
+// EMAIL: yanyinliu8@gmail.com
+// ID: 604952257
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <time.h>
+#include <getopt.h>
+#include <string.h>
+#include <errno.h>
+#include <sched.h> 
+#define THREADS 't'
+#define ITERATIONS 'i'
+#define YIELD 'y'
+#define SYNC 's'
+
+#define BILLION  1000000000L
+
+// void add(long long *pointer, long long value) {
+//     long long sum = *pointer + value;
+//     *pointer = sum;
+// }
+
+//  initialize a mutex variable:
+pthread_mutex_t mymutex = PTHREAD_MUTEX_INITIALIZER;
+int lock = 0;
+int nthread = 1, niterate = 1;
+struct timespec start, stop;
+
+struct threadargs{
+	long long *counter;
+	int niterate;
+	char sync_option;
+};
+
+int opt_yield;
+void add(long long *pointer, long long value) {
+    long long sum = *pointer + value;
+    if (opt_yield)
+            sched_yield();
+    *pointer = sum;
+}
+
+void m_add(long long *pointer, long long value)
+{
+	pthread_mutex_lock(&mymutex);
+	long long sum = *pointer + value;
+    if (opt_yield)
+            sched_yield();
+    *pointer = sum;
+	pthread_mutex_unlock(&mymutex);
+}
+
+void s_add(long long *pointer, long long value)
+{
+	while (__sync_lock_test_and_set(&lock, 1));
+	long long sum = *pointer + value;
+    if (opt_yield)
+            sched_yield();
+    *pointer = sum;
+	__sync_lock_release(&lock);
+}
+
+void c_add(long long *pointer, long long value)
+{
+	int new, old;
+	do
+	{
+		old = *pointer;
+		if (opt_yield)
+            sched_yield();
+		new = old + value;
+	}while(__sync_val_compare_and_swap(pointer, old, new) != old);
+}
+
+void* my_add(void *th_args)
+{
+	struct threadargs *data = (struct threadargs*) th_args;
+	long long *counter = data->counter;
+	int niterate = data->niterate;
+	char sync_option = data->sync_option;
+	//long long old, new;
+	int i,j;
+	for (i=0; i< niterate; i++)
+	{
+		if((sync_option) == 'm')
+		{
+			m_add(counter, 1);
+		}
+		else if(sync_option == 's')
+		{
+			s_add(counter, 1);
+		}
+		else if(sync_option == 'c')
+		{
+			c_add(counter, 1);
+		}
+		else if(sync_option == ' ')
+		{
+			add(counter, 1);
+		}
+	}
+
+
+	for (j=0; j< (data->niterate); j++)
+	{
+		if((data->sync_option) == 'm')
+		{
+			m_add(counter, -1);
+		}
+		else if((data->sync_option) == 's')
+		{
+			s_add(counter, -1);
+		}
+		else if((data->sync_option) == 'c')
+		{
+			c_add(counter, -1);
+		}
+		else if((data->sync_option) == ' ')
+		{
+			add(counter, -1);
+		}
+	}
+	// exits to re-join the parent thread
+	pthread_exit(0);
+}
+
+
+int main(int argc, char **argv)
+{	
+	// int nthread = 1, niterate = 1;
+	// struct timespec start, stop;
+	//int opt; // use to get the option characters
+	// int yieldflag = 0;
+	long long counter = 0;
+	char sync_option = ' ';
+	int c,i;
+	//char *name = (char*) malloc(32*sizeof(char));
+	static struct option long_options[]=
+	{
+		{"threads", 1, NULL, THREADS},
+		{"iterations", 1, NULL, ITERATIONS},
+		{"yield", 0, NULL, YIELD},
+		{"sync", 1, NULL, SYNC},
+		{0,0,0,0}
+	};
+	while(1)
+	{
+		c = getopt_long(argc, argv, "", long_options, NULL);
+		if(c == -1)
+		{
+			break;
+		}
+		//fprintf(stderr, "c: %c, optind: %d, optarg: %s\n", c, optind, optarg);
+		switch(c)
+		{
+			case THREADS:
+				nthread = atoi(optarg);
+				if(nthread < 1)
+				{
+					fprintf(stderr, "Error! Invalid thread number: %s\n", strerror(errno));
+					exit(1);
+				}
+				break;
+			case ITERATIONS: 
+				niterate = atoi(optarg);
+				if(niterate < 1)
+				{
+					fprintf(stderr, "Error! Invalid iteration number: %s\n", strerror(errno));
+					exit(1);
+				}
+				break;
+			case YIELD:
+				opt_yield = 1;
+				break;
+			case SYNC:
+				sync_option = (char)* optarg;
+				if((sync_option != 'm') && (sync_option != 's') && (sync_option != 'c'))
+				{
+					fprintf(stderr, "Error! Invalid sync option: %s\n", strerror(errno));
+					exit(1);
+				}
+				
+				
+				break;
+			default:
+				fprintf(stderr, "Error! Unrecognized argument: %s\n", argv[optind-1]);
+				exit(1);
+		}
+	}
+
+
+	
+	struct threadargs th_args;
+	th_args.counter = &counter;
+	th_args.niterate = niterate;
+	th_args.sync_option = sync_option;
+
+	if(clock_gettime(CLOCK_MONOTONIC, &start) == -1)
+	{
+		fprintf(stderr, "Error on starting time for the run!\n");
+		exit(1);
+	}
+
+	pthread_t thread[nthread];
+	int rc;
+	for(i=0; i<nthread; i++)
+	{
+		rc = pthread_create(&thread[i], NULL, my_add, (void*) &th_args);
+		if(rc)
+		{
+			fprintf(stderr, "Error! Return code from pthread_create(): %d\n", rc);
+			exit(1);
+		}
+	}
+
+	for(i=0; i<nthread; i++)
+	{
+		pthread_join(thread[i], NULL);
+	}
+
+	if(clock_gettime(CLOCK_MONOTONIC, &stop) == -1)
+	{
+		fprintf(stderr, "Error on stopping time for the run!\n");
+		exit(1);
+	}
+
+	//prints to stdout a comma-separated-value (CSV)
+	// name = "add-none";
+	switch(sync_option)
+	{
+		case 'm': 
+			if(opt_yield)
+			{
+				fprintf(stdout, "add-yield-m,");
+			}
+			else
+			{
+				fprintf(stdout, "add-m,");
+			}
+			break;
+		case 's':
+			if(opt_yield)
+			{
+				fprintf(stdout, "add-yield-s,");
+			}
+			else
+			{
+				fprintf(stdout,"add-s,");
+			}
+			break;
+		case 'c':
+			if(opt_yield)
+			{
+				fprintf(stdout,"add-yield-c,");
+			}
+			else
+			{
+				fprintf(stdout, "add-c,");
+			}
+			break;
+		// case ' ':
+		// 	if(opt_yield)
+		// 	{
+		// 		name = "add-yield-none";
+		// 	}
+		// 	else
+		// 	{
+		// 		name = "add-none";
+		// 	}
+		// 	break;
+		default: 
+			if(opt_yield)
+			{
+				fprintf(stdout,"add-yield-none,");
+			}
+			else
+			{
+				fprintf(stdout,"add-none,");
+			}
+	}
+
+	long long num_op = nthread * niterate * 2;
+	long long runtime = BILLION * (stop.tv_sec - start.tv_sec) + (stop.tv_nsec - start.tv_nsec);
+	long long avg_time_per_oper = runtime / num_op;
+
+	printf("%d,%d,%lld,%lld,%lld,%lld\n", nthread, niterate, num_op, runtime, avg_time_per_oper, counter);
+	if(counter != 0)
+		exit(2);
+
+	exit(0);
+}
+
+
+
---
+++ lab2a/test.sh
@@ -0,0 +1,158 @@
+#!/bin/bash
+
+# NAME: Yanyin Liu
+# EMAIL: yanyinliu8@gmail.com
+# ID: 604952257
+
+
+# Run your program for ranges of iterations (100, 1000, 10000, 100000) values (with no thread and no yield)
+./lab2_add --iterations=100 >> lab2_add.csv
+./lab2_add --iterations=1000 >> lab2_add.csv
+./lab2_add --iterations=10000 >> lab2_add.csv
+./lab2_add --iterations=100000 >> lab2_add.csv
+
+# Re-run your tests, with yields, for ranges of threads (2,4,8,12) and iterations (10, 20, 40, 80, 100, 1000, 10000, 100000)
+# lab2_add-1.png
+./lab2_add --threads=2 --iterations=10 --yield >> lab2_add.csv
+./lab2_add --threads=2 --iterations=20 --yield >> lab2_add.csv
+./lab2_add --threads=2 --iterations=40 --yield >> lab2_add.csv
+./lab2_add --threads=2 --iterations=80 --yield >> lab2_add.csv
+./lab2_add --threads=2 --iterations=100 --yield >> lab2_add.csv
+./lab2_add --threads=2 --iterations=1000 --yield >> lab2_add.csv
+./lab2_add --threads=2 --iterations=10000 --yield >> lab2_add.csv
+./lab2_add --threads=2 --iterations=100000 --yield >> lab2_add.csv
+
+./lab2_add --threads=4 --iterations=10 --yield >> lab2_add.csv
+./lab2_add --threads=4 --iterations=20 --yield >> lab2_add.csv
+./lab2_add --threads=4 --iterations=40 --yield >> lab2_add.csv
+./lab2_add --threads=4 --iterations=80 --yield >> lab2_add.csv
+./lab2_add --threads=4 --iterations=100 --yield >> lab2_add.csv
+./lab2_add --threads=4 --iterations=1000 --yield >> lab2_add.csv
+./lab2_add --threads=4 --iterations=10000 --yield >> lab2_add.csv
+./lab2_add --threads=4 --iterations=100000 --yield >> lab2_add.csv
+
+./lab2_add --threads=8 --iterations=10 --yield >> lab2_add.csv
+./lab2_add --threads=8 --iterations=20 --yield >> lab2_add.csv
+./lab2_add --threads=8 --iterations=40 --yield >> lab2_add.csv
+./lab2_add --threads=8 --iterations=80 --yield >> lab2_add.csv
+./lab2_add --threads=8 --iterations=100 --yield >> lab2_add.csv
+./lab2_add --threads=8 --iterations=1000 --yield >> lab2_add.csv
+./lab2_add --threads=8 --iterations=10000 --yield >> lab2_add.csv
+./lab2_add --threads=8 --iterations=100000 --yield >> lab2_add.csv
+
+./lab2_add --threads=12 --iterations=10 --yield >> lab2_add.csv
+./lab2_add --threads=12 --iterations=20 --yield >> lab2_add.csv
+./lab2_add --threads=12 --iterations=40 --yield >> lab2_add.csv
+./lab2_add --threads=12 --iterations=80 --yield >> lab2_add.csv
+./lab2_add --threads=12 --iterations=100 --yield >> lab2_add.csv
+./lab2_add --threads=12 --iterations=1000 --yield >> lab2_add.csv
+./lab2_add --threads=12 --iterations=10000 --yield >> lab2_add.csv
+./lab2_add --threads=12 --iterations=100000 --yield >> lab2_add.csv
+
+# threads (2, 8) and of iterations (100, 1000, 10000, 100000), non yield vs with yield
+# lab2_add-2.png
+./lab2_add --threads=2 --iterations=100 >> lab2_add.csv
+./lab2_add --threads=2 --iterations=100 --yield >> lab2_add.csv
+./lab2_add --threads=2 --iterations=1000 >> lab2_add.csv
+./lab2_add --threads=2 --iterations=1000 --yield >> lab2_add.csv
+./lab2_add --threads=2 --iterations=10000 >> lab2_add.csv
+./lab2_add --threads=2 --iterations=10000 --yield >> lab2_add.csv
+./lab2_add --threads=2 --iterations=100000 >> lab2_add.csv
+./lab2_add --threads=2 --iterations=100000 --yield >> lab2_add.csv
+
+./lab2_add --threads=8 --iterations=100 >> lab2_add.csv
+./lab2_add --threads=8 --iterations=100 --yield >> lab2_add.csv
+./lab2_add --threads=8 --iterations=1000 >> lab2_add.csv
+./lab2_add --threads=8 --iterations=1000 --yield >> lab2_add.csv
+./lab2_add --threads=8 --iterations=10000 >> lab2_add.csv
+./lab2_add --threads=8 --iterations=10000 --yield >> lab2_add.csv
+./lab2_add --threads=8 --iterations=100000 >> lab2_add.csv
+./lab2_add --threads=8 --iterations=100000 --yield >> lab2_add.csv
+
+#synchronization option (none, mutex, spin, compare-and-swap), with yield
+#iterations (10,000 for mutexes and CAS, only 1,000 for spin locks)
+#lab2_add-4.png
+for t in 2 4 8 12
+do
+	./lab2_add --threads=$t --iterations=10000 --yield --sync=m >> lab2_add.csv
+	./lab2_add --threads=$t --iterations=10000 --yield --sync=c >> lab2_add.csv
+	./lab2_add --threads=$t --iterations=1000 --yield --sync=s >> lab2_add.csv
+done
+
+#for a range of number of threads (1,2,4,8,12), no yield, large enough number of iterations (e.g. 10,000) t
+#lab2_add-5.png
+for t in 1 2 4 8 12
+do
+	for s in 'm' 's' 'c'
+	do
+		./lab2_add --threads=$t --iterations=10000 --sync=$s >> lab2_add.csv
+	done
+	
+done
+
+
+
+
+# with a single thread, and increasing numbers of iterations (10, 100, 1000, 10000, 20000)
+# lab2_list-1.png
+./lab2_list --threads=1  --iterations=10 >> lab2_list.csv
+./lab2_list --threads=1  --iterations=100 >> lab2_list.csv
+./lab2_list --threads=1  --iterations=1000 >> lab2_list.csv
+./lab2_list --threads=1  --iterations=10000	>> lab2_list.csv
+./lab2_list --threads=1  --iterations=20000	>> lab2_list.csv
+
+ #using various combinations of yield options and see how many threads (2,4,8,12) and iterations (1, 2,4,8,16,32)
+for i in 1 2 4 8 16 32
+do
+	for t in 2 4 8 12
+	do
+		for y in 'i' 'id' 'idl' 'il' 'dl' 'd' 'l'
+		do
+			./lab2_list --threads=$t --iterations=$i --yield=$y >> lab2_list.csv
+		done
+	done
+done
+
+# pthread_mutexes (--sync=m), and another protected by test-and-set spin locks (--sync=s).
+# numbers of threads (2) and iterations (16)
+# numbers of threads (12) and iterations (32)
+# lab2_list-3.png.
+
+./lab2_list --threads=2 --iterations=16 --yield=i  --sync=m >> lab2_list.csv
+./lab2_list --threads=2 --iterations=16 --yield=d  --sync=m >> lab2_list.csv
+./lab2_list --threads=2 --iterations=16 --yield=il --sync=m >> lab2_list.csv
+./lab2_list --threads=2 --iterations=16 --yield=dl --sync=m >> lab2_list.csv
+./lab2_list --threads=2 --iterations=16 --yield=i  --sync=s >> lab2_list.csv
+./lab2_list --threads=2 --iterations=16 --yield=d  --sync=s >> lab2_list.csv
+./lab2_list --threads=2 --iterations=16 --yield=il --sync=s >> lab2_list.csv
+./lab2_list --threads=2 --iterations=16 --yield=dl --sync=s >> lab2_list.csv
+
+./lab2_list --threads=12 --iterations=32 --yield=i  --sync=m >> lab2_list.csv
+./lab2_list --threads=12 --iterations=32 --yield=d  --sync=m >> lab2_list.csv
+./lab2_list --threads=12 --iterations=32 --yield=il --sync=m >> lab2_list.csv
+./lab2_list --threads=12 --iterations=32 --yield=dl --sync=m >> lab2_list.csv
+./lab2_list --threads=12 --iterations=32 --yield=i  --sync=s >> lab2_list.csv
+./lab2_list --threads=12 --iterations=32 --yield=d  --sync=s >> lab2_list.csv
+./lab2_list --threads=12 --iterations=32 --yield=il --sync=s >> lab2_list.csv
+./lab2_list --threads=12 --iterations=32 --yield=dl --sync=s >> lab2_list.csv
+
+# iterations (e.g. 1000), threads (1, 2, 4, 8, 12, 16, 24), synchronization options: mutex, spin
+# lab2_list-4.png
+./lab2_list --threads=1 --iterations=1000 --sync=m >> lab2_list.csv
+./lab2_list --threads=2 --iterations=1000 --sync=m >> lab2_list.csv
+./lab2_list --threads=4 --iterations=1000 --sync=m >> lab2_list.csv
+./lab2_list --threads=8 --iterations=1000 --sync=m >> lab2_list.csv
+./lab2_list --threads=12 --iterations=1000 --sync=m >> lab2_list.csv
+./lab2_list --threads=16 --iterations=1000 --sync=m >> lab2_list.csv
+./lab2_list --threads=24 --iterations=1000 --sync=m >> lab2_list.csv
+
+./lab2_list --threads=1 --iterations=1000 --sync=s >> lab2_list.csv
+./lab2_list --threads=2 --iterations=1000 --sync=s >> lab2_list.csv
+./lab2_list --threads=4 --iterations=1000 --sync=s >> lab2_list.csv
+./lab2_list --threads=8 --iterations=1000 --sync=s >> lab2_list.csv
+./lab2_list --threads=12 --iterations=1000 --sync=s >> lab2_list.csv
+./lab2_list --threads=16 --iterations=1000 --sync=s >> lab2_list.csv
+./lab2_list --threads=24 --iterations=1000 --sync=s >> lab2_list.csv
+
+
+
