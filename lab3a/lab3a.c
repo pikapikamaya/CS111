795a3873b283f6c071b9f1534ef2c5b49dacb8fa
pikapikamaya <38804662+pikapikamaya@users.noreply.github.com>
2018-06-19 11:53:09 -0700

    Update lab3a.c

--- lab3a/lab3a.c
+++ lab3a/lab3a.c
@@ -15,28 +15,24 @@
 #include <time.h>
 #include "ext2_fs.h"
 
-/*superblock*/
-#define SUPER_OFFSET 1024 //The superblock is always located at byte offset 1024 from the beginning of the file
+
+#define SUPER_OFFSET 1024 
 void * buffer;
 struct ext2_super_block super;
-int imgfd; // global .img file descriptor
+int imgfd;
 int num_block;
 int num_inode;
-int blocksize; // global
-int inodesize; // global
+int blocksize; 
+int inodesize; 
 unsigned int block_per_group;
-unsigned int inode_per_group; // global
-/*superblock*/
+unsigned int inode_per_group; 
 
-/* Group */
-int groupnum; // for the total number of group
+int groupnum; 
 struct ext2_group_desc* group = NULL;
 
-// ======= Auxillary Functions ======
+
 char * timetoString(__u32 given_time) {
-  //take care, this function mallocs
-  //person who this is returned to must free the char*
-  //when he/she is done with it
+  
   int itim = (int) given_time;
   time_t tim = (time_t) itim;
   struct tm * time = gmtime(&tim);
@@ -46,16 +42,13 @@ char * timetoString(__u32 given_time) {
       fprintf(stderr, "Malloc Failed\n");
       exit(2);
     }
-  //note: tm_mon goes from 0 to 11, so to get the actual month add 1
+  
   sprintf(timeString, "%02d/%02d/%02d %02d:%02d:%02d",time->tm_mon + 1, time->tm_mday, time->tm_year % 100, time->tm_hour, time->tm_min, time->tm_sec);
   return timeString;
 }
-
-// ======= Superblock Summary =======
-//SHOULD WORK
 void Superblock_Summary()
 {
-	// open the file and use pread to read sth out
+	
 	if(pread(imgfd, buffer, sizeof(struct ext2_super_block), SUPER_OFFSET) > 0)
 	{
 	  struct ext2_super_block * temp = (struct ext2_super_block *) buffer;
@@ -79,13 +72,11 @@ void Superblock_Summary()
 }
 
 
-// ======= Group Summary =======
-//SHOULD WORK
 void Group_Summary()
 {
 	unsigned int rem_blocks = num_block % block_per_group;
 	unsigned int rem_inodes = num_inode % inode_per_group;
-	// get the group number
+	
 	groupnum = 1 + (super.s_blocks_count-1) / super.s_blocks_per_group;
 	int i;
 	for (i=0; i<groupnum; i++)
@@ -104,9 +95,6 @@ void Group_Summary()
 
 }
 
-// ======= Free Block Entries ======
-// ======= Free I-node Entries ======
-//SHOULD WORK
 void Free_Block_Entries()
 {
 	__u8 *blocks;
@@ -150,7 +138,6 @@ void Free_Block_Entries()
 			int k; 
 			for (k = 0; k<8; k++)
 			{
-				// 1 means "used" and 0 "free/available",
 				if ((checkbit & checkbit_block) == 0)
 				{
 					fprintf(stdout, "BFREE,%u\n", (k + 1) + (8 * j) + i * super.s_blocks_per_group);
@@ -169,13 +156,10 @@ void Free_Block_Entries()
 
 }
 
-//======================Finding Indirectly Located Directories============
-//look deeper for directory entries in indirect locations, code taken from a combination of the Indirect Summary and Directory
 void Dir_Indirect_Summary(struct ext2_inode * inode, unsigned int inode_num, int level_of_indirection, __u32 block_num, unsigned int size) {
   if (block_num == 0)
     return;
-  int total = blocksize/sizeof(__u32); //number of blocks
-  //can't use global buffer due to possible recusive call/general call problems from other functions
+  int total = blocksize/sizeof(__u32); 
   __u32 buffert [1024];
   int readErr = pread(imgfd, buffert, blocksize, SUPER_OFFSET + (block_num -1)*blocksize);
   if (readErr == -1)
@@ -193,7 +177,7 @@ void Dir_Indirect_Summary(struct ext2_inode * inode, unsigned int inode_num, int
   for (l = 0; l < total; l++) {
     if (block_ptr[l] == 0)
       continue;   
-    if (level_of_indirection > 1) //recursively dig
+    if (level_of_indirection > 1) 
       Dir_Indirect_Summary(inode, inode_num, level_of_indirection - 1, block_ptr[l], size);
     ssize_t c = pread(imgfd, &direct, blocksize, blocksize * (block_ptr[l] -1) + 1024);
     if (c<0)
@@ -201,47 +185,37 @@ void Dir_Indirect_Summary(struct ext2_inode * inode, unsigned int inode_num, int
 	fprintf(stderr, "Error on reading directory inode: %s\n", strerror(errno));
 	exit(2);
       }
-    // from header file
+    
     direct_ptr = (struct ext2_dir_entry *) direct;
-    //unsigned int size = 0; //logical byte offset (decimal) of this entry within the directory
+    
     for(;;)
       {
-	if ((direct_ptr->inode == 0) || ((int)size >= blocksize)) //nonvalid or we hit the end of the block
+	if ((direct_ptr->inode == 0) || ((int)size >= blocksize)) 
 	  break;
-	// get the file name and memcpy it
-	int len = direct_ptr->name_len;
-	char filename[len + 1]; //goes from 0 to len
 	
-	//strcpy(filename, direct_ptr->name);
-	//do this instead because TA noted that strcpy and printf might fail if the null byte is corrupted or missing
+	int len = direct_ptr->name_len;
+	char filename[len + 1]; 
 	memcpy(filename, direct_ptr->name, len);
-	filename[len] = '\0'; //append null byte
-	if(direct_ptr != NULL) //I don't think it'll ever be NULL ...
+	filename[len] = '\0'; 
+	if(direct_ptr != NULL) 
 	  {
 	    fprintf(stdout, "DIRENT,%u,%u,%u,%u,%u,'%s'\n", inode_num, size, direct_ptr->inode, direct_ptr->rec_len, direct_ptr->name_len, filename);
 	  }
-	//reorder
 	size += direct_ptr->rec_len;
-	direct_ptr = (void *) direct_ptr + direct_ptr->rec_len;      /* move to the next entry */
+	direct_ptr = (void *) direct_ptr + direct_ptr->rec_len;    
       }
   }
 }
 
-
-// ======= Directory Entries =======
-//SEEMS TO WORK
 void Directory_Summary(struct ext2_inode *dir_inode, unsigned int inode_num)
 {
-	// 32-bit value representing the total number of 512-bytes blocks reserved to contain the data of this inode, 
-	// regardless if these blocks are used or not. 
-	//int maxindex = dir_inode->i_blocks / (2<<blocksize);
 	int i;
 	for(i=0; i < 15; i++)
 	{
 	  if (dir_inode->i_block[i] == 0)
 	    continue;
 	  unsigned int size = 0;
-	  if(i<=11) // go up to 11 because need to take care of special 12, 13, and 14 blocks
+	  if(i<=11) 
 		{
 			char direct[blocksize];
 			ssize_t c = pread(imgfd, &direct, blocksize, blocksize * (dir_inode->i_block[i] -1) + 1024);
@@ -250,28 +224,23 @@ void Directory_Summary(struct ext2_inode *dir_inode, unsigned int inode_num)
 				fprintf(stderr, "Error on reading directory inode: %s\n", strerror(errno));
 				exit(2);
 			}
-			// from header file
 			struct ext2_dir_entry *direct_ptr = (struct ext2_dir_entry *) direct;
-			//unsigned int size = 0; //logical byte offset (decimal) of this entry within the directory
 			for(;;)
 			{
-			  if ((direct_ptr->inode == 0) || ((int)size >= blocksize)) //nonvalid or we hit the end of the block
+			  if ((direct_ptr->inode == 0) || ((int)size >= blocksize))
 			    break;
-			  // get the file name and memcpy it
+			 
 			  int len = direct_ptr->name_len;
-			  char filename[len + 1]; //goes from 0 to len
-			  
-			  //strcpy(filename, direct_ptr->name);
-			  //do this instead because TA noted that strcpy and printf might fail if the null byte is corrupted or missing
-			  memcpy(filename, direct_ptr->name, len);
-			  filename[len] = '\0'; //append null byte
-			  if(direct_ptr != NULL) //I don't think it'll ever be NULL ...
+			  char filename[len + 1];
+			   memcpy(filename, direct_ptr->name, len);
+			  filename[len] = '\0';
+			  if(direct_ptr != NULL) 
 			    {
 			      fprintf(stdout, "DIRENT,%u,%u,%u,%u,%u,'%s'\n", inode_num, size, direct_ptr->inode, direct_ptr->rec_len, direct_ptr->name_len, filename);
 			    }
-			  //reorder
+			  
 			  size += direct_ptr->rec_len;
-			  direct_ptr = (void *) direct_ptr + direct_ptr->rec_len;      /* move to the next entry */
+			  direct_ptr = (void *) direct_ptr + direct_ptr->rec_len;
 			}
 		
 		} 
@@ -281,23 +250,14 @@ void Directory_Summary(struct ext2_inode *dir_inode, unsigned int inode_num)
 	    Dir_Indirect_Summary(dir_inode, inode_num, 2, dir_inode->i_block[13], size);
 	  else if (i == 14)
 	    Dir_Indirect_Summary(dir_inode, inode_num, 3, dir_inode->i_block[14], size);
-	  /* We're not going to print out the error message because there isn't a strerror to print out...
-		else
-		{
-			fprintf(stderr, "Directory_Summary index is larger than 14: %s\n", strerror(errno));
-			exit(2);
-		}
-	  */
 	}
 }
 
-// ======= Indirect Block References =======
-// should work now
+
 void Indirect_Summary(unsigned int inode_num, __u32 totalprev, int level_of_indirection, __u32 block_num) {
   if (block_num == 0)
     return;
-  int total = blocksize/sizeof(__u32); //number of blocks
-  //can't use global buffer due to possible recusive call/general call problems from other functions
+  int total = blocksize/sizeof(__u32); 
   __u32 buffert [1024];
   int readErr = pread(imgfd, buffert, blocksize, SUPER_OFFSET + (block_num -1)*blocksize);
   if (readErr == -1)
@@ -307,7 +267,7 @@ void Indirect_Summary(unsigned int inode_num, __u32 totalprev, int level_of_indi
       exit(2);
     }
   __u32 * block_ptr = (__u32 *) buffert; 
-  int l; //what a horrible choice of variable
+  int l; 
   for (l = 0; l < total; l++) {
     if (block_ptr[l] == 0)
       continue;   
@@ -316,17 +276,11 @@ void Indirect_Summary(unsigned int inode_num, __u32 totalprev, int level_of_indi
       Indirect_Summary(inode_num, totalprev + l, level_of_indirection - 1, block_ptr[l]);
   }
 }
-
-// ======= I-node Summary =======
-//SHOULD WORK
 void Inode_Summary() {
   int i;
-  for (i = 0; i < groupnum; i++) //for each group
+  for (i = 0; i < groupnum; i++) 
     {
       int grouploc = blocksize * group[i].bg_inode_table;
-
-      //for each inode block in the group
-      //I need a new buffer or it'll actually override the previous group buffer
       void * buffa = malloc(1024); 
       if (buffa == NULL)
 	{
@@ -351,22 +305,22 @@ void Inode_Summary() {
 	      int file = 0x8000;
 	      int directory = 0x4000;
 	      int symbolic_link = 0xA000;
-	      int mode_upper = mode & 0xF000; //grab uppermost 4 bits
+	      int mode_upper = mode & 0xF000; 
 	      if (mode_upper == file)
 	      {
 	      	filetype = 'f';
-		Indirect_Summary(j + 1, 12, 1, inode->i_block[12]); //intial offset
-		Indirect_Summary(j + 1, 12 + 256, 2, inode->i_block[13]); //one block of bytes
-		Indirect_Summary(j + 1, 12 + 256 + (256*256), 3, inode->i_block[14]); //two blocks of blocks of additional offset
+		Indirect_Summary(j + 1, 12, 1, inode->i_block[12]); 
+		Indirect_Summary(j + 1, 12 + 256, 2, inode->i_block[13]);
+		Indirect_Summary(j + 1, 12 + 256 + (256*256), 3, inode->i_block[14]); 
 	      }
 	      else if (mode_upper == directory)
 	      {
 	      	filetype = 'd';
-	      	// should call the Indirect Block References
+	      	
 	      	Directory_Summary(inode, j+1);
-		Indirect_Summary(j + 1, 12, 1, inode->i_block[12]); //intial offset
-		Indirect_Summary(j + 1, 12 + 256, 2, inode->i_block[13]); //one block of bytes
-		Indirect_Summary(j + 1, 12 + 256 + (256*256), 3, inode->i_block[14]); //two blocks of blocks of additional ofset
+		Indirect_Summary(j + 1, 12, 1, inode->i_block[12]); 
+		Indirect_Summary(j + 1, 12 + 256, 2, inode->i_block[13]); 
+		Indirect_Summary(j + 1, 12 + 256 + (256*256), 3, inode->i_block[14]);
 	      }
 			
 	      else if (mode_upper == symbolic_link)
@@ -376,14 +330,14 @@ void Inode_Summary() {
 			
 	      else
 		filetype = '?';
-	      int mode_lower8 = mode & 0x0FFF; //grab lower order 12 bits
-	      char * lastInodeChange = timetoString(inode->i_ctime); //metadata change
-	      char * modificationTime = timetoString(inode->i_mtime); //data change
+	      int mode_lower8 = mode & 0x0FFF; 
+	      char * lastInodeChange = timetoString(inode->i_ctime); 
+	      char * modificationTime = timetoString(inode->i_mtime); 
 	      char * accessTime = timetoString(inode->i_atime);
-		//until the problem with inode modification is resolved I'll use the access time for both I guess
-	      if ((filetype == 'f') || (filetype == 'd')) //inode at j = 0 is the 1st inode
+		
+	      if ((filetype == 'f') || (filetype == 'd')) 
 		fprintf(stdout, "INODE,%d,%c,%o,%u,%u,%u,%s,%s,%s,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u\n", j + 1, filetype, mode_lower8, inode->i_uid, inode->i_gid, inode->i_links_count, lastInodeChange, modificationTime, accessTime, inode->i_size, inode->i_blocks, inode->i_block[0], inode->i_block[1], inode->i_block[2], inode->i_block[3], inode->i_block[4], inode->i_block[5], inode->i_block[6], inode->i_block[7], inode->i_block[8], inode->i_block[9], inode->i_block[10], inode->i_block[11], inode->i_block[12], inode->i_block[13], inode->i_block[14]); 
-	      else if (inode->i_size < 60) //file size less than block size, symbolic link case
+	      else if (inode->i_size < 60)
 		fprintf(stdout, "INODE,%d,%c,%o,%u,%u,%u,%s,%s,%s,%u,%u,%u\n", j + 1, filetype, mode_lower8, inode->i_uid, inode->i_gid, inode->i_links_count, lastInodeChange, modificationTime, accessTime, inode->i_size, inode->i_blocks, inode->i_block[0]); 
 	      else 
 		fprintf(stderr, "Unknown type of large file (> 60 bytes)\n");
@@ -396,7 +350,6 @@ void Inode_Summary() {
     }
 }
 
-// ======= main =======
 int main(int argc, char **argv)
 {
 	if(argc != 2)
@@ -405,8 +358,7 @@ int main(int argc, char **argv)
 	    fprintf(stderr, "Usage: lab4b [file.img]\n");
 	    exit(1);
 	  }
-  
-	//check to see that the last item in argv[1] is .img, 
+
 	int arglen = strlen(argv[1]);
 	if ((argv[1][arglen -1] != 'g') || (argv[1][arglen -2] != 'm') || (argv[1][arglen -3] != 'i') || (argv[1][arglen -4] != '.'))
 	  {
@@ -414,7 +366,7 @@ int main(int argc, char **argv)
 	    exit(1);
 	  }
 
-	//this means that argv[1] contains the file name
+	
 	int opErr = open(argv[1], O_RDONLY);
 	if (opErr == -1)
 	  {
@@ -422,23 +374,17 @@ int main(int argc, char **argv)
 	    fprintf(stderr, "Failure to open image file: %s\n", strerror(err));
 	    exit(2);
 	  }
-	imgfd = opErr; //returned fd of the file
-
-	//malloc massive buffer once
-	buffer = malloc(1024); //now buffer will always have the malloc
+	imgfd = opErr; 
+	buffer = malloc(1024); 
 	if (buffer == NULL)
 	  {
 	    int err = errno;
 	    fprintf(stderr, "Error on malloc: %s\n", strerror(err));
 	  }
-
-	//Do Summaries in order
 	Superblock_Summary();
 	Group_Summary();
 	Free_Block_Entries();
 	Inode_Summary();
-
-	//free buffer
 	free(buffer);
 
 	exit(0);
